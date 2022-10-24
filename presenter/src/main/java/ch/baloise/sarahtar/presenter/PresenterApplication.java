3db231cef19bbe254718f01687d4aca9ea174bcf
Christoph Lipp <c.lipp@gmx.net>
2022-10-24 18:32:49 +0200

    add presenter module

---
+++ presenter/src/main/java/ch/baloise/sarahtar/presenter/PresenterApplication.java
@@ -0,0 +1,13 @@
+package ch.baloise.sarahtar.presenter;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class PresenterApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(PresenterApplication.class, args);
+    }
+
+}
---
+++ presenter/src/test/java/ch/baloise/sarahtar/presenter/PresenterApplicationTests.java
@@ -0,0 +1,15 @@
+package ch.baloise.sarahtar.presenter;
+
+import org.junit.jupiter.api.Disabled;
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+//@SpringBootTest
+class PresenterApplicationTests {
+
+    @Disabled
+    @Test
+    void contextLoads() {
+    }
+
+}
