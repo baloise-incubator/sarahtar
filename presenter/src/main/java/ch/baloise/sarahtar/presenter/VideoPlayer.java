65d1e29d1234d91cf7254c158fa30ea87bfb7de6
daniel-st <daniel.stoll@bluewin.ch>
2022-10-25 16:52:55 +0200

    Use placeholders for command construction

--- presenter/src/main/java/ch/baloise/sarahtar/presenter/VideoPlayer.java
+++ presenter/src/main/java/ch/baloise/sarahtar/presenter/VideoPlayer.java
@@ -8,27 +8,32 @@ import java.io.IOException;
 @Service
 public class VideoPlayer {
 
-    private static final String RPI_COMMAND = "mplayer -fs -vfm ffmpeg -idle -fixed-vo";
+    private static final String SPEECH_PLACEHOLDER = "<<speech>>";
+    private static final String IDLE_PLACEHOLDER = "<<idle>>";
+    private static final String RPI_COMMAND = "mplayer -fs "+SPEECH_PLACEHOLDER+" -fs -idle -fixed-vo "+IDLE_PLACEHOLDER+" -loop 0";
     private static final String HOMEDIR = "/usr/bin/sarahtar/videos/";
 
     Logger logger = LoggerFactory.getLogger(VideoPlayer.class);
 
-    public String playVideo(String fileName) throws IOException {
-        logger.info("Received play command for video: " + fileName);
+    public String playVideo(String speachFileName, String idleFileName) throws IOException {
+        logger.info("Received play command for video: " + speachFileName);
 
         if (!isUnix()) {
             return "Unsupported operating system: Video not started";
         }
 
-        String cmdString = RPI_COMMAND + " "+ HOMEDIR + fileName;
-        String[] cmdArray = cmdString.split( " ");
+        String cmdString = RPI_COMMAND
+                .replace(SPEECH_PLACEHOLDER,HOMEDIR + speachFileName)
+                .replace(IDLE_PLACEHOLDER,HOMEDIR + idleFileName);
 
-        ProcessBuilder pb = new ProcessBuilder(cmdArray);
+        logger.info("Using command: " + cmdString);
+
+        ProcessBuilder pb = new ProcessBuilder(cmdString.split( " "));
         // local display
         pb.environment().put("DISPLAY", ":0");
 
         pb.start();
-        return "Started Video: " + fileName;
+        return "Started Video: " + speachFileName;
     }
 
     private static boolean isUnix() {
