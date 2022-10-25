a2a0b43e0b9c4d31d81993329fbb39a889cb8fc5
Christoph Lipp <c.lipp@gmx.net>
2022-10-25 14:14:34 +0200

    add module sensor-trigger

---
+++ sensor-trigger/src/main/java/ch/baloise/sarahtar/sensortrigger/SensorTriggerApplication.java
@@ -0,0 +1,13 @@
+package ch.baloise.sarahtar.sensortrigger;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SensorTriggerApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(SensorTriggerApplication.class, args);
+    }
+
+}
