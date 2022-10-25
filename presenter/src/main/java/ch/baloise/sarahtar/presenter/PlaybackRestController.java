ecd1165d8476ab6e257280f412ae1164a1c59be5
Mateusz Milart <mateusz.milart@baloise.ch>
2022-10-25 11:54:40 +0200

    Add Logging and change to start shell script for init display

--- presenter/src/main/java/ch/baloise/sarahtar/presenter/PlaybackRestController.java
+++ presenter/src/main/java/ch/baloise/sarahtar/presenter/PlaybackRestController.java
@@ -1,5 +1,7 @@
 package ch.baloise.sarahtar.presenter;
 
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RequestMapping;
@@ -12,6 +14,8 @@ import java.io.IOException;
 @RequestMapping("play/")
 public class PlaybackRestController {
 
+    Logger logger = LoggerFactory.getLogger(PlaybackRestController.class);
+
     @GetMapping("/{id}/{videoid}/{language}")
     public String playVideo(@PathVariable int id, @PathVariable int videoid, @PathVariable String language) throws IOException {
         return playVideoById(id, videoid, language);
@@ -19,15 +23,16 @@ public class PlaybackRestController {
 
     @PostConstruct
     public void init() throws IOException {
-        Runtime.getRuntime().exec("sh export DISPLAY=:0");
+        Runtime.getRuntime().exec("sh /usr/bin/sarahtar/initdisplay.sh");
     }
 
     private String playVideoById(int id, int videoid, String language) throws IOException {
         String rpiCommand = "mplayer -fs -vfm ffmpeg -idle -fixed-vo";
-        String filename = "Avatar"+id+"_Video"+videoid+"_"+language;
+        String filename = "Avatar"+id+"_Video"+videoid+"_"+language+".mp4";
         String homedir = "/usr/bin/sarahtar/videos/";
         String rpiCommandPlayback = rpiCommand + " " + homedir + filename;
-        Process process = Runtime.getRuntime().exec(rpiCommandPlayback);
+        logger.info("Received play command: video: " + id +", avatar: " + videoid + " to start once");
+        Runtime.getRuntime().exec(rpiCommandPlayback);
         return "Video: " + id +", avatar: " + videoid + " has been startet once";
     }
 }
