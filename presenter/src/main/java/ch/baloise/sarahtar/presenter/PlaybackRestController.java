7e65a04df7cf96b55cd0906593f9eb76b18430f0
Mateusz Milart <mateusz.milart@baloise.ch>
2022-10-25 10:46:33 +0200

    Add PlaybackRestController

---
+++ presenter/src/main/java/ch/baloise/sarahtar/presenter/PlaybackRestController.java
@@ -0,0 +1,19 @@
+@RestController
+@RequestMapping("play/")
+public class PlaybackRestController {
+
+    @GetMapping("/{id}/{videoid}/{language}", produces = "application/text")
+    public String playVideo(@PathVariable int id) {
+        return playVideoById(id, videoid, language);
+    }
+
+    private String playVideoById(int id, String videoid, String language) {
+        String rpiCommand = "mplayer -fs -vfm ffmpeg -idle -fixed-vo mplayer -fs"
+        String filename = "Avatar"+id+"_Video"+videoid+"_"+language;
+        String homedir = "/usr/bin/sarathar/videos/";
+        String rpiCommandPlayback = rpiCommand + " " + homedir + filename;
+        Process process = Runtime.getRuntime().exec(rpiCommandPlayback);
+        return "Video: " + id +", avatar: " + avatarName + " has been startet once";
+    }
+
+}
