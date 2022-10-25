bbf085825a28b0ecef3f05e2f808b97845a3e7ff
daniel-st <daniel.stoll@bluewin.ch>
2022-10-25 15:05:08 +0200

    Use ProcessBuilder to start mplayer with DISPLAY environment variable

--- presenter/src/main/java/ch/baloise/sarahtar/presenter/PresenterApplication.java
+++ presenter/src/main/java/ch/baloise/sarahtar/presenter/PresenterApplication.java
@@ -1,11 +1,7 @@
 package ch.baloise.sarahtar.presenter;
 
-import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
-
-import javax.annotation.PostConstruct;
-import java.io.IOException;
 
 @SpringBootApplication
 public class PresenterApplication {
--- presenter/src/main/java/ch/baloise/sarahtar/presenter/VideoPlayer.java
+++ presenter/src/main/java/ch/baloise/sarahtar/presenter/VideoPlayer.java
@@ -2,28 +2,34 @@ package ch.baloise.sarahtar.presenter;
 
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
-import org.springframework.stereotype.Component;
 import org.springframework.stereotype.Service;
-import org.springframework.web.bind.annotation.GetMapping;
-import org.springframework.web.bind.annotation.PathVariable;
-import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RestController;
-
-import javax.annotation.PostConstruct;
 import java.io.IOException;
 
 @Service
 public class VideoPlayer {
 
-    private static final String RPI_COMMAND = "mplayer -fs -vfm ffmpeg -idle -fixed-vo";
+    //private static final String RPI_COMMAND = "mplayer -fs -vfm ffmpeg -idle -fixed-vo";
     private static final String HOMEDIR = "/usr/bin/sarahtar/videos/";
 
     Logger logger = LoggerFactory.getLogger(VideoPlayer.class);
 
     public String playVideo(String fileName) throws IOException {
         logger.info("Received play command for video: " + fileName);
-        String rpiCommandPlayback = RPI_COMMAND + " " + HOMEDIR + fileName;
-        Runtime.getRuntime().exec(rpiCommandPlayback);
+
+        String[] rpiCommandPlayback = {
+                "/usr/bin/mplayer",
+                "-fs",
+                "-vfm",
+                "ffmpeg",
+                "-idle",
+                "-fixed-vo",
+                HOMEDIR + fileName
+        };
+
+        ProcessBuilder pb = new ProcessBuilder(rpiCommandPlayback);
+        pb.environment().put("DISPLAY", ":0");
+
+        pb.start();
         return "Started Video: " + fileName;
     }
 }
