f19cae3e7a4de22e40f72da0506bd4ca2b0a787e
Mateusz Milart <mateusz.milart@baloise.ch>
2022-10-25 11:12:50 +0200

    Fix Controller

--- presenter/src/main/java/ch/baloise/sarahtar/presenter/PlaybackRestController.java
+++ presenter/src/main/java/ch/baloise/sarahtar/presenter/PlaybackRestController.java
@@ -1,19 +1,28 @@
+package ch.baloise.sarahtar.presenter;
+
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+import java.io.IOException;
+
 @RestController
 @RequestMapping("play/")
 public class PlaybackRestController {
 
-    @GetMapping("/{id}/{videoid}/{language}", produces = "application/text")
-    public String playVideo(@PathVariable int id) {
+    @GetMapping("/{id}/{videoid}/{language}")
+    public String playVideo(@PathVariable int id, @PathVariable int videoid, @PathVariable String language) throws IOException {
         return playVideoById(id, videoid, language);
     }
 
-    private String playVideoById(int id, String videoid, String language) {
-        String rpiCommand = "mplayer -fs -vfm ffmpeg -idle -fixed-vo mplayer -fs"
+    private String playVideoById(int id, int videoid, String language) throws IOException {
+        String rpiCommand = "mplayer -fs -vfm ffmpeg -idle -fixed-vo mplayer -fs";
         String filename = "Avatar"+id+"_Video"+videoid+"_"+language;
         String homedir = "/usr/bin/sarahtar/videos/";
         String rpiCommandPlayback = rpiCommand + " " + homedir + filename;
         Process process = Runtime.getRuntime().exec(rpiCommandPlayback);
-        return "Video: " + id +", avatar: " + avatarName + " has been startet once";
+        return "Video: " + id +", avatar: " + videoid + " has been startet once";
     }
 
 }
